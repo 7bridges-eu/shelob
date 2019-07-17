af2176215bd3379e45bf70ad03ceffa5c3b6fa4e
Marco Dalla Stella <marco@7bridges.eu>
2019-07-17 12:36:42 +0200

    Add multiple features:
    - wait conditions
    - children of an element

--- src/shelob/core.clj
+++ src/shelob/core.clj
@@ -1,6 +1,7 @@
 (ns shelob.core
   (:import [org.openqa.selenium WebDriver By]
-           [org.openqa.selenium.firefox FirefoxDriver]))
+           [org.openqa.selenium.firefox FirefoxDriver]
+           [org.openqa.selenium.support.ui WebDriverWait ExpectedConditions]))
 
 (defn web-driver
   []
@@ -11,14 +12,30 @@
   (.get driver url)
   driver)
 
+(defn expected-condition
+  [condition locator]
+  (case condition
+    :visibility (ExpectedConditions/visibilityOfElementLocated locator)))
+
+(defn wait-for
+  ([driver condition locator]
+   (wait-for driver condition locator 2))
+  ([driver condition locator timeout]
+   (let [wdw (WebDriverWait. driver timeout)
+         ec (expected-condition condition locator)]
+     (.until wdw ec))))
+
 (defn by
-  [{:keys [what how]}]
-  (case what
-    :id (By/id how)
-    :link-text (By/linkText how)
-    :name (By/name how)
-    :class-name (By/className how)
-    :xpath (By/xpath how)))
+  [context query]
+  (case context
+    :class-name (By/className query)
+    :css-selector (By/cssSelector query)
+    :id (By/id query)
+    :link-text (By/linkText query)
+    :name (By/name query)
+    :partial-link-text (By/partialLinkText query)
+    :tag-name (By/tagName query)
+    :xpath (By/xpath query)))
 
 (defn find-element
   [starting-point by]
@@ -27,6 +44,14 @@
 (defn find-elements
   [starting-point by]
   (.findElements starting-point by))
+
+(defn children
+  [starting-point]
+  (find-elements starting-point (by :xpath ".//*")))
+
+(defn children-by
+  [starting-point locator]
+  (find-elements starting-point locator))
 
 (defn fill
   [element text]
