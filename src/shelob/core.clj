8ec0f948f9fd9e83f333340398790048256dda3f
Manuel Uberti <manuel.uberti@inventati.org>
2019-07-25 12:26:04 +0200

    Add timbre and configure file appender

--- src/shelob/core.clj
+++ src/shelob/core.clj
@@ -1,1 +1,6 @@
-(ns shelob.core)
+(ns shelob.core
+  (:require [taoensso.timbre :as timbre]
+            [taoensso.timbre.appenders.core :as appenders]))
+
+(timbre/merge-config!
+ {:appenders {:spit (appenders/spit-appender {:fname "shelob.log"})}})
