a732434f525b33ca9f59c2279ee0503aef87ead1
Marco Dalla Stella <marco@7bridges.eu>
2019-08-22 16:16:06 +0200

    Implements a command execution architecture for browsing.

--- src/shelob/temp.clj
+++ src/shelob/temp.clj
@@ -18,21 +18,40 @@
   [pool]
   (doseq [[k driver] pool]
     (println "Closing " k)
-    (.quit (:driver driver)))
+    (.quit (:instance driver)))
   (reset! driver-pool {}))
 
-(defn available-driver
+(defn- available-driver
   [pool]
   (->> pool
        (filter (fn [[_ v]] (:available v)))
        first))
+
+(defn take-driver [pool]
+  (let [[driver-id driver] (available-driver @pool)]
+    (swap! pool update-in [driver-id :available] not)
+    [driver-id (:instance driver)]))
+
+(defn release-driver [pool driver-id]
+  (swap! pool update-in [driver-id :available] not))
+
+(defn- init-channels [ctx]
+  (assoc ctx :channels {:messages (as/chan)
+                        :scraper (as/chan)
+                        :results (as/chan)}))
+
+(defn- close-channels [ctx]
+  (let [channels (:channels ctx)]
+    (doseq [channel (vals channels)]
+      (as/close! channel)))
+  (dissoc ctx :channels))
 
 (defn- ->proxy [http ssl]
   (-> (Proxy.)
       (.setHttpProxy http)
       (.setSslProxy ssl)))
 
-(defmulti driver-options :driver)
+(defmulti driver-options :browser)
 (defmethod driver-options :firefox [options]
   (let [default-options (-> (FirefoxOptions.)
                             (.setHeadless true))]
@@ -47,35 +66,99 @@
 (defmethod driver-options :edge [options]
   (throw (ex-info "Edge not implemented yet!" {})))
 
+(defn chrome-driver
+  [options]
+  (System/setProperty "webdriver.chrome.silentLogging" "true")
+  (System/setProperty "webdriver.chrome.silentOutput" "true")
+  (-> options
+      (.setHeadless true)
+      (ChromeDriver.)))
+
+(defn edge-driver
+  [options]
+  (EdgeDriver. options))
+
+(defn firefox-driver
+  [options]
+  (System/setProperty FirefoxDriver$SystemProperty/BROWSER_LOGFILE "/dev/null")
+  (-> options
+      (.setHeadless true)
+      (FirefoxDriver.)))
+
+(defn internet-explorer-driver
+  [options]
+  (InternetExplorerDriver. options))
+
+(defn opera-driver
+  [options]
+  (OperaDriver. options))
+
+(defn safari-driver
+  [options]
+  (SafariDriver. options))
+
+(defn web-driver
+  [browser options]
+  (case browser
+    :chrome (chrome-driver options)
+    :edge (edge-driver options)
+    :firefox (firefox-driver options)
+    :internet-explorer (internet-explorer-driver options)
+    :opera (opera-driver options)
+    :safari (safari-driver options)
+    (firefox-driver options)))
+
 (defn init-driver [options]
   (let [opts (driver-options options)
-        driver-type (:driver options)
-        driver (case driver-type
-                 :firefox (FirefoxDriver. opts)
-                 :chrome (ChromeDriver. opts)
-                 :edge (EdgeDriver. opts)
-                 (throw (ex-info (str "Invalid browser type: " driver-type) {})))]
-    driver))
+        browser (:browser options)]
+    (web-driver browser opts)))
 
 (defn init-driver-pool [ctx]
   (let [pool-size (:pool-size ctx 5)]
     (dotimes [_ pool-size]
       (let [options (:driver-options ctx)
             driver (init-driver options)]
-        (swap! driver-pool assoc (.hashCode driver) {:driver driver
+        (swap! driver-pool assoc (.hashCode driver) {:instance driver
                                                      :available true})))
     ctx))
 
+(defn- to-vector [x]
+  (if (vector? x) x (vector x)))
+
+(defn- exec-listener
+  "Create a command executor listener. Messages should always be vectors."
+  [ctx]
+  (let [in-ch (get-in ctx [:channels :messages])
+        scrapers-ch (get-in ctx [:channels :scraper])]
+    (as/go-loop []
+      (when-let [messages (as/<! in-ch)]
+        (let [[driver-id driver] (take-driver driver-pool)]
+          (doseq [message messages]
+            (println "Sending" message)
+            (->> (assoc message :driver driver)
+                 shc/browser-command
+                 (as/>! scrapers-ch)))
+          (release-driver driver-pool driver-id)
+          (recur))))))
+
+(defn init-executors [ctx]
+  (exec-listener ctx)
+  ctx)
+
 (defn init [ctx]
   (-> ctx
-      init-driver-pool))
+      init-driver-pool
+      init-channels
+      init-executors))
 
 (defn example []
-  (let [context (init {:driver-options {:driver :firefox}})
-        in-chan (get-in context [:channels :browser-in])
-        out-chan (get-in context [:channels :browser-out])]
-    (as/onto-chan in-chan [{:msg :go :url "https://7bridges.eu"}
-                           {:msg :get-title}])
+  (let [context (init {:driver-options {:browser :firefox}
+                       :pool-size 2})
+        in-chan (get-in context [:channels :messages])
+        out-chan (get-in context [:channels :scraper])]
+    (as/onto-chan in-chan [[{:msg :go :url "https://7bridges.eu"}
+                            {:msg :source}]])
     (as/go-loop []
-      (println (as/<! out-chan))
-      (recur))))
+      (when-let [v (as/<! out-chan)]
+        (println v)
+        (recur)))))
